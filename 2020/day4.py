09a3eafaf001b3386310a0b8085f2f53eb50274e
mickeypash <pashov.m@gmail.com>
2020-12-18 22:23:22 +0000

    Day4 clean up

--- 2020/day4.py
+++ 2020/day4.py
@@ -1,44 +1,4 @@
 
-"""
-byr (Birth Year)
-iyr (Issue Year)
-eyr (Expiration Year)
-hgt (Height)
-hcl (Hair Color)
-ecl (Eye Color)
-pid (Passport ID)
-cid (Country ID)
-"""
-"""
-ATTRS = ["byr", "iyr", "eyr", "hgt","hcl","ecl","pid","cid"]
-def search(line):
-    query = {i:False for i in ATTRS}
-
-    tokens = line.split(" ")
-    for token in tokens:
-        key, value = token.split(":")
-        if key in query.keys():
-            query[key] = True
-    return query
-
-f = open("input4.txt")
-
-lines = f.read()
-
-test = []
-for line in lines.split("\n")[:5]:
-
-    if line == "":
-        continue
-    else:
-        print(search(line))
-        test.append(line)
-
-print(test)
-
-f.close()
-"""
-import json
 
 ATTRS = ["byr", "iyr", "eyr", "hgt","hcl","ecl","pid"]
 def is_valid_passport(passport):
