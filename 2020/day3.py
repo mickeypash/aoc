b5e4869fd80595b41bae80a510060d39bf9df1a8
mickeypash <pashov.m@gmail.com>
2020-12-18 16:49:55 +0000

    Day3 Part2

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,9 +1,35 @@
 
-HIT  = "X"
-MISS = "O"
-
 TREE = "#"
 SNOW = "."
+
+def part2(lines, right, down):
+
+    count_hits = 0
+    count_misses = 0
+    count = 0
+
+    for row in range(0, len(lines), down):
+        
+        col = count * right % len(lines[row])
+        something = lines[row][col]
+        #print(lines[row])
+        if something == TREE:
+            count_hits += 1
+        if something == SNOW:
+            count_misses += 1
+
+        count +=1
+
+    return count_hits
+
+def test_slopes(slopes):
+    multiplicative_hits = 1
+
+    for slope in slopes:
+        multiplicative_hits *= (part2(lines, *slope))
+
+    return multiplicative_hits
+
 
 if __name__ == '__main__':
 
@@ -11,19 +37,21 @@ if __name__ == '__main__':
 
     lines = f.read().splitlines()
 
-    count_hits = 0
-    count_misses = 0
+    slopes = [
+            (1, 1),
+            (3, 1),
+            (5, 1),
+            (7, 1),
+            (1, 2)
+    ]
 
-    for i in range(0, len(lines)):
-        idx = i*3 % len(lines[i]) 
-        something = lines[i][idx]
-        if something == TREE:
-            print(something)
-            count_hits += 1
-        if something == SNOW:
-            count_misses += 1
-
-    print(count_hits)
-    print(count_misses)
+    print(test_slopes(slopes))
 
     f.close()
+
+"""
+.##......#.##..#..#..##....#...
+#....##....#.....#.....#..##.##
+##..........#....#.#...#.......
+.#.##........#..............#..
+"""
