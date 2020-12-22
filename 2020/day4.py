76be87488f8de1b41c4b9122c2baf3717bc307ea
mickeypash <pashov.m@gmail.com>
2020-12-22 20:38:04 +0200

    Fix silly mistake with Day 4

--- 2020/day4.py
+++ 2020/day4.py
@@ -1,11 +1,7 @@
 import re
 
-FIELDS = ["byr", "iyr", "eyr", "hgt","hcl","ecl","pid"]
 def is_valid_passport(passport):
-    for attr in FIELDS:
-        if attr not in passport.keys():
-            return False
-    return True
+    return len(passport.keys()) == 8 or len(passport.keys()) == 7 and "cid" not in passport.keys()
 
 def is_between(_min, field, _max):
     return _min <= int(field) <= _max
@@ -22,7 +18,7 @@ def is_valid_hgt(passport):
             return is_between(59, height, 76)
     return False
 
-def is_valid_passport2(passport):
+def is_valid_passport_strict(passport):
     """
         byr (Birth Year) - four digits; at least 1920 and at most 2002.
         iyr (Issue Year) - four digits; at least 2010 and at most 2020.
@@ -35,23 +31,26 @@ def is_valid_passport2(passport):
         pid (Passport ID) - a nine-digit number, including leading zeroes.
         cid (Country ID) - ignored, missing or not.
     """
-    has_fields = len(passport.keys()) <= 8 or len(passport.keys()) <= 7 and "cid" not in passport.keys()
     if not is_valid_passport(passport): return False
 
     byr = has_digits(passport["byr"], 4) and is_between(1920, passport["byr"], 2002)
     iyr = has_digits(passport["iyr"], 4) and is_between(2010, passport["iyr"], 2020)
-    eyr = has_digits(passport["iyr"], 4) and is_between(2020, passport["iyr"], 2020)
+    eyr = has_digits(passport["iyr"], 4) and is_between(2020, passport["eyr"], 2030)
     htg = is_valid_hgt(passport)
-    hcl = bool(re.match(r"^#([0-9a-f]{6,})$", passport["hcl"]))
+    hcl = bool(re.match(r"^#([0-9a-f]{6,6})$", passport["hcl"]))
     ecl = passport["ecl"] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
     pid = len(passport["pid"]) == 9 and passport["pid"].isdigit()
 
-    return byr and iyr and eyr and htg and hcl and ecl and pid
+    return byr and iyr and eyr and htg and hcl and ecl and pid 
 
-with open('input4.txt', 'r') as file:
-    lst = file.read().split('\n\n')
-    lst = [x.replace('\n', ' ').split() for x in lst]
 
-    passports = [dict(p.split(":") for p in l) for l in lst]
+if __name__ == '__main__':
+    with open('input4.txt', 'r') as file:
+        lst = file.read().split('\n\n')
+        lst = [x.replace('\n', ' ').split() for x in lst]
 
-    print(sum([is_valid_passport2(p) for p in passports]))
+        passports = [dict(p.split(":") for p in l) for l in lst]
+
+        print(sum([is_valid_passport(p) for p in passports]))
+
+        print(sum([is_valid_passport_strict(p) for p in passports]))
