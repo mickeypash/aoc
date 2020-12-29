761bcaa765348a417380c7f8d50b6e6f51d9d5fe
mickeypash <pashov.m@gmail.com>
2020-12-29 22:34:29 +0200

    Day 5 Part 2 - bit of a sham but it works

--- 2020/day5.py
+++ 2020/day5.py
@@ -1,3 +1,13 @@
 with open("input5.txt") as f:
-    print(max([int("".join(["0" if c == "F" or c == "L" else "1" for c in line]), 2) for line in f.read().splitlines()]))
+    seat_ids = [int("".join(["0" if c == "F" or c == "L" else "1" for c in line]), 2) for line in f.read().splitlines()]
+    # part 1
+    print(max(seat_ids))
 
+    sorted_seat_ids = sorted(seat_ids)
+    starting = sorted_seat_ids[0]
+
+    # part 2 
+    for i, seat_id in enumerate(sorted_seat_ids, starting):
+        if i < seat_id:
+            print(i)
+            break
